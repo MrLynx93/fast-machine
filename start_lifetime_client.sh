bf253180f222c98480f20d121b6cabc6f0234cba
MrLynx <mateusz.ziebura93@gmail.com>
2017-06-03 22:39:49 +0200

    changed slleppptime

--- start_lifetime_client.sh
+++ start_lifetime_client.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$2" == "coap" ]
-then
-#    mvn clean install
-    pushd integration-tests
-    for ((i=1; i<=$1; i++))
-    do
-        mvn exec:java -Dexec.mainClass="com.test.client." -Dexec.args="$i $3" > "logs/client_$i.log" 2>1 &
-    done
-    popd
-fi
+
+pushd integration-tests
+mvn exec:java -Dexec.mainClass="com.test.lifetime.StartCoapClient" -Dexec.args="$1" > "logs/client_lifetime_$1.log" 2>1 &
+popd
