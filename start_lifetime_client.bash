7825acecfa1d99764e6d34a522593307b259698b
MrLynx <mateusz.ziebura93@gmail.com>
2017-06-04 12:47:48 +0200

    Chenged sequence regex

--- start_lifetime_client.bash
+++ start_lifetime_client.bash
@@ -5,7 +5,7 @@ if [ "$2" == "mqtt" ]
 then
     pushd ././../lynx-lwm2m/
     make clean all
-    make runtest ARGS="costam 0" 2>&1 | tee "logs/client_lifetime_$1.log"
+    make runtest ARGS="costam 0" 2>&1 | tee "logs/client_mqtt_lifetime_$1.log"
     popd
 fi
 
@@ -13,7 +13,7 @@ if [ "$2" == "coap" ]
 then
 #    mvn clean install
     pushd integration-tests
-    mvn exec:java -Dexec.mainClass="com.test.lifetime.StartCoapClient" -Dexec.args="$1" 2>&1 | tee "logs/client_lifetime_$1.log"
+    mvn exec:java -Dexec.mainClass="com.test.lifetime.StartCoapClient" -Dexec.args="$1" 2>&1 | tee "logs/client_coap_lifetime_$1.log"
     popd
 fi
 
