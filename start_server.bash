05dd2ae2ddcc00213a115dce158e593ef54a21d2
MrLynx <mateusz.ziebura93@gmail.com>
2017-06-05 20:51:54 +0200

    Fixed lifetime tests for MQTTasdas

--- start_server.bash
+++ start_server.bash
@@ -6,12 +6,12 @@ pushd integration-tests
 
 if [ "$2" == "mqtt" ]
 then
-    mvn exec:java -Dexec.mainClass="com.test.server.MqttStartTestServer" -Dexec.args="$1 $3" > logs/server_mqtt.log 2>1
+    mvn exec:java -Dexec.mainClass="com.test.server.MqttStartTestServer" -Dexec.args="$1 $3" 2>&1 | tee logs/server_mqtt.log
 fi
 
 if [ "$2" == "coap" ]
 then
-    mvn exec:java -Dexec.mainClass="com.test.server.CoapStartTestServer" -Dexec.args="$1 $3" > logs/server_coap.log 2>1
+    mvn exec:java -Dexec.mainClass="com.test.server.CoapStartTestServer" -Dexec.args="$1 $3" 2>&1 | tee logs/server_coap.log
 fi
 
 popd
